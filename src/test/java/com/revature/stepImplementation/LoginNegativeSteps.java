74e21e732c52f129d1e9a45abe673f44233415cd
Jonathan Whitmire <jkwhitmi@gmail.com>
2022-11-26 14:30:31 -0600

    more stepimplementation files

--- src/test/java/com/revature/stepImplementation/LoginNegativeSteps.java
+++ src/test/java/com/revature/stepImplementation/LoginNegativeSteps.java
@@ -1,5 +1,6 @@
 package com.revature.stepImplementation;
 
+import com.revature.runners.Runner;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,38 +12,31 @@ public class LoginNegativeSteps {
 
     @Given("The user is on the login page")
     public void the_user_is_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Runner.driver.get("http://localhost:63342/MicroBlaug/src/main/webApp/html/login-signup.html");
     }
     @When("The user types freebird into the username input")
     public void the_user_types_freebird_into_the_username_input() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Runner.loginPage.usernameInput.sendKeys("freebird");
     }
     @When("The user types pass4321 into the password input")
     public void the_user_types_pass4321_into_the_password_input() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Runner.loginPage.passwordInput.sendKeys("pass4321");
     }
     @When("The user types greebird into the username input")
     public void the_user_types_greebird_into_the_username_input() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Runner.loginPage.usernameInput.sendKeys("greebird");
     }
     @When("The user types pass1234 into the password input")
     public void the_user_types_pass1234_into_the_password_input() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Runner.loginPage.passwordInput.sendKeys("pass1234");
     }
     @When("The user types  into the username input")
     public void the_user_types_into_the_username_input() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Runner.loginPage.usernameInput.sendKeys("");
     }
     @When("The user types  into the password input")
     public void the_user_types_into_the_password_input() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Runner.loginPage.passwordInput.sendKeys("");
     }
     @When("The user clicks the Sign in button")
     public void the_user_clicks_the_sign_in_button() {
