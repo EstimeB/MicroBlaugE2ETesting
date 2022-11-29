a2504fe33a9b01dbf09be66179016a1fcff3a071
Dwyan Alford <dwyan.alford@gmail.com>
2022-11-28 22:44:29 -0800

    comments e2e testing now complete 6 test cases all passing here are final files

--- src/test/java/com/revature/stepsImplementation/comment/CommentsSteps.java
+++ src/test/java/com/revature/stepsImplementation/comment/CommentsSteps.java
@@ -4,6 +4,7 @@ import com.revature.runners.CommentsRunner;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
@@ -12,7 +13,7 @@ import static com.revature.pages.CommentsPage.Home_URL;
 import static com.revature.pages.CommentsPage.Login_URL;
 import static com.revature.runners.CommentsRunner.driver;
 
-public class CommentsPositiveSteps {
+public class CommentsSteps {
 
     @Given("The user lands on the login page")
     public void the_user_lands_on_the_login_page() throws InterruptedException {
@@ -22,8 +23,9 @@ public class CommentsPositiveSteps {
     }
 
     @When("The user types {string} into username input")
-    public void the_user_types_into_username_input(String string) {
+    public void the_user_types_into_username_input(String string) throws InterruptedException {
         CommentsRunner.CommentsPage.username.sendKeys(string);
+        Thread.sleep(500);
 
     }
 
@@ -33,8 +35,9 @@ public class CommentsPositiveSteps {
 
     }
     @When("The user clicks the Sign in button")
-    public void the_user_clicks_the_sign_in_button() {
+    public void the_user_clicks_the_sign_in_button() throws InterruptedException {
         CommentsRunner.CommentsPage.login.click();
+        Thread.sleep(500);
 
     }
 
@@ -46,56 +49,69 @@ public class CommentsPositiveSteps {
 
     @When("The user types a Comment into the input field with")
     public void the_user_types_a_comment_into_the_input_field_with(String docString) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         CommentsRunner.CommentsPage.newCommentMessage.sendKeys("This is the best Post I have come across today!");
-
     }
 
     @Then("The user clicks the save button")
-    public void the_user_clicks_the_save_button() {
+    public void the_user_clicks_the_save_button() throws InterruptedException {
         CommentsRunner.CommentsPage.saveCommentButton.click();
+        Thread.sleep(500);
 
     }
 
     @When("The user confirms by pressing ok")
     public void the_user_confirms_by_pressing_ok() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.switchTo().alert().accept();
     }
 
     @Then("The Newly created Comment should appear at the top of that Post's comments")
-    public void the_newly_created_comment_should_appear_at_the_top_of_that_post_s_comments() {
+    public void the_newly_created_comment_should_appear_at_the_top_of_that_post_s_comments() throws InterruptedException {
+        Thread.sleep(500);
         String commentText = CommentsRunner.CommentsPage.firstCommentRow.getText();
         Assert.assertEquals(commentText, "This is the best Post I have come across today!");
+        Thread.sleep(1000);
     }
 
     @When("The user presses on the trash can icon next to a comment on a post")
-    public void the_user_presses_on_the_trash_can_icon_next_to_a_comment_on_a_post() {
+    public void the_user_presses_on_the_trash_can_icon_next_to_a_comment_on_a_post() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         CommentsRunner.CommentsPage.deleteCommentIcon.click();
+        Thread.sleep(750);
     }
 
     @When("The user presses on the edit icon next to a comment on a post")
     public void the_user_presses_on_the_edit_icon_next_to_a_comment_on_a_post() {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         CommentsRunner.CommentsPage.editCommentIcon.click();
     }
 
     @Then("The user presses on the update icon next to the comment on a post")
     public void the_user_presses_on_the_update_icon_next_to_the_comment_on_a_post() {
-        CommentsRunner.CommentsPage.updateCommentInput.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        CommentsRunner.CommentsPage.updateCommentIcon.click();
     }
 
-    @Then("An Alert to say {string}")
-    public void an_alert_say(String expectedText) {
+    @Then("An Alert say {string}")
+    public void an_alert_say(String expectedText) throws InterruptedException {
+        Thread.sleep(1000);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         String actualText = driver.switchTo().alert().getText();
         Assert.assertEquals(actualText, expectedText);
     }
 
-    @Then("An Alert that reads {string}")
+    @Then("An Alert reads {string}")
     public void an_alert_reads(String expectedText) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         String actualText = driver.switchTo().alert().getText();
         Assert.assertEquals(actualText, expectedText);
     }
 
     @Then("An Alert says {string}")
-    public void an_alert_says(String expectedText) {
+    public void an_alert_says(String expectedText) throws InterruptedException {
+        Thread.sleep(3000);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         String actualText = driver.switchTo().alert().getText();
         Assert.assertEquals(actualText, expectedText);
     }
